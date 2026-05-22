5f3698662423bc0e5b571f178b308b08567a6c32
RSenaDev <rsena53535@gmail.com>
2026-05-22 10:20:16 +0100

    Update: src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
+import { useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import Portfolio from './pages/Portfolio'
 
+function useCircularFavicon(src) {
+  useEffect(() => {
+    const img = new Image()
+    img.src = src
+    img.onload = () => {
+      const canvas = document.createElement('canvas')
+      canvas.width = 64
+      canvas.height = 64
+      const ctx = canvas.getContext('2d')
+      ctx.beginPath()
+      ctx.arc(32, 32, 32, 0, Math.PI * 2)
+      ctx.closePath()
+      ctx.clip()
+      ctx.drawImage(img, 0, 0, 64, 64)
+      const link = document.querySelector("link[rel~='icon']")
+      if (link) link.href = canvas.toDataURL('image/png')
+    }
+  }, [src])
+}
+
 export default function App() {
+  useCircularFavicon('/logo.jpeg')
   return (
     <>
       <Navbar />
